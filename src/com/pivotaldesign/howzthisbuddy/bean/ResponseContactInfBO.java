aa15edfc0072c5089a7577e530f371dabe868560
Rajesh <rajesh.budem@gmail.com>
2015-08-30 16:13:28 +0530

    Rajesh - integrated merchant app

--- src/com/pivotaldesign/howzthisbuddy/bean/ResponseContactInfBO.java
+++ src/com/pivotaldesign/howzthisbuddy/bean/ResponseContactInfBO.java
@@ -4,6 +4,13 @@ public class ResponseContactInfBO {
 
 	private String phonenum;
 	private String name;
+	private boolean ischecked;
+	public boolean isIschecked() {
+		return ischecked;
+	}
+	public void setIschecked(boolean ischecked) {
+		this.ischecked = ischecked;
+	}
 	public String getPhonenum() {
 		return phonenum;
 	}
