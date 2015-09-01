bf02b9f859b5772c39649d247a5b70f774411605
Rajesh <rajesh.budem@gmail.com>
2015-09-02 00:38:09 +0530

    Rajesh Changes

--- src/com/pivotaldesign/howzthisbuddy/bean/ResponseContactInfBO.java
+++ src/com/pivotaldesign/howzthisbuddy/bean/ResponseContactInfBO.java
@@ -5,23 +5,29 @@ public class ResponseContactInfBO {
 	private String phonenum;
 	private String name;
 	private boolean ischecked;
+	
 	public boolean isIschecked() {
 		return ischecked;
 	}
+
 	public void setIschecked(boolean ischecked) {
 		this.ischecked = ischecked;
 	}
+
 	public String getPhonenum() {
 		return phonenum;
 	}
+
 	public void setPhonenum(String phonenum) {
 		this.phonenum = phonenum;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 }
